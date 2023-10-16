6ce7a3794d4476f8b6892d6b3b2b47dc7869a2fc
Lisoveliy <1986developer@gmail.com>
2023-10-16 21:06:10 +0300

    Refactor

---
+++ wwwroot/scripts/ButtonLoading.js
@@ -0,0 +1,4 @@
+function showLoad() {
+            document.getElementById("submit").style = 'display: none'
+            document.getElementById("loadingbutton").style = 'width: 10em; margin: auto'
+}
